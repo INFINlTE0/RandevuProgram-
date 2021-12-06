f1d5457c27956ba5a7812e9dc8829171c3c4f204
Ali <79650377+INFINlTE0@users.noreply.github.com>
2021-12-06 20:31:21 +0300

    Add files via upload

---
+++ Form3.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace Randevu_Sistemi
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.monthCalendar_randevuTarihleri = new System.Windows.Forms.MonthCalendar();
+            this.lblHosgeldinizAciklama = new System.Windows.Forms.Label();
+            this.picBoxTarihAciklama = new System.Windows.Forms.PictureBox();
+            this.llblTarihAciklama = new System.Windows.Forms.Label();
+            this.lblTarih1 = new System.Windows.Forms.Label();
+            this.lblTarih2 = new System.Windows.Forms.Label();
+            this.lblTarihveSonrasiniAciklama1 = new System.Windows.Forms.Label();
+            this.btnRandevularım = new System.Windows.Forms.Button();
+            this.btnCikis = new System.Windows.Forms.Button();
+            this.lblTarihveSonrasiniAciklama2 = new System.Windows.Forms.Label();
+            this.Hakan = new System.Windows.Forms.Label();
+            this.Çalışan1 = new System.Windows.Forms.PictureBox();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxTarihAciklama)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Çalışan1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.monthCalendar_randevuTarihleri);
+            this.panel2.Controls.Add(this.lblHosgeldinizAciklama);
+            this.panel2.Location = new System.Drawing.Point(2, 203);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(290, 234);
+            this.panel2.TabIndex = 37;
+            // 
+            // monthCalendar_randevuTarihleri
+            // 
+            this.monthCalendar_randevuTarihleri.BackColor = System.Drawing.Color.DimGray;
+            this.monthCalendar_randevuTarihleri.FirstDayOfWeek = System.Windows.Forms.Day.Monday;
+            this.monthCalendar_randevuTarihleri.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.monthCalendar_randevuTarihleri.ForeColor = System.Drawing.Color.White;
+            this.monthCalendar_randevuTarihleri.Location = new System.Drawing.Point(9, 9);
+            this.monthCalendar_randevuTarihleri.MaxSelectionCount = 1;
+            this.monthCalendar_randevuTarihleri.Name = "monthCalendar_randevuTarihleri";
+            this.monthCalendar_randevuTarihleri.TabIndex = 21;
+            this.monthCalendar_randevuTarihleri.TitleBackColor = System.Drawing.Color.Red;
+            this.monthCalendar_randevuTarihleri.TitleForeColor = System.Drawing.Color.White;
+            this.monthCalendar_randevuTarihleri.TrailingForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.monthCalendar_randevuTarihleri.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar_randevuTarihleri_DateChanged);
+            // 
+            // lblHosgeldinizAciklama
+            // 
+            this.lblHosgeldinizAciklama.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblHosgeldinizAciklama.AutoSize = true;
+            this.lblHosgeldinizAciklama.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblHosgeldinizAciklama.ForeColor = System.Drawing.Color.Black;
+            this.lblHosgeldinizAciklama.Location = new System.Drawing.Point(6, 209);
+            this.lblHosgeldinizAciklama.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblHosgeldinizAciklama.Name = "lblHosgeldinizAciklama";
+            this.lblHosgeldinizAciklama.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblHosgeldinizAciklama.Size = new System.Drawing.Size(10, 15);
+            this.lblHosgeldinizAciklama.TabIndex = 46;
+            this.lblHosgeldinizAciklama.Text = ".";
+            this.lblHosgeldinizAciklama.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // picBoxTarihAciklama
+            // 
+            this.picBoxTarihAciklama.Image = ((System.Drawing.Image)(resources.GetObject("picBoxTarihAciklama.Image")));
+            this.picBoxTarihAciklama.Location = new System.Drawing.Point(313, 248);
+            this.picBoxTarihAciklama.Name = "picBoxTarihAciklama";
+            this.picBoxTarihAciklama.Size = new System.Drawing.Size(690, 25);
+            this.picBoxTarihAciklama.TabIndex = 41;
+            this.picBoxTarihAciklama.TabStop = false;
+            // 
+            // llblTarihAciklama
+            // 
+            this.llblTarihAciklama.AutoSize = true;
+            this.llblTarihAciklama.BackColor = System.Drawing.SystemColors.Control;
+            this.llblTarihAciklama.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.llblTarihAciklama.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.llblTarihAciklama.Image = ((System.Drawing.Image)(resources.GetObject("llblTarihAciklama.Image")));
+            this.llblTarihAciklama.Location = new System.Drawing.Point(321, 253);
+            this.llblTarihAciklama.Name = "llblTarihAciklama";
+            this.llblTarihAciklama.Size = new System.Drawing.Size(98, 15);
+            this.llblTarihAciklama.TabIndex = 42;
+            this.llblTarihAciklama.Text = "Tarih                    :";
+            // 
+            // lblTarih1
+            // 
+            this.lblTarih1.AutoSize = true;
+            this.lblTarih1.BackColor = System.Drawing.SystemColors.Control;
+            this.lblTarih1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTarih1.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.lblTarih1.Image = ((System.Drawing.Image)(resources.GetObject("lblTarih1.Image")));
+            this.lblTarih1.Location = new System.Drawing.Point(416, 253);
+            this.lblTarih1.Name = "lblTarih1";
+            this.lblTarih1.Size = new System.Drawing.Size(116, 15);
+            this.lblTarih1.TabIndex = 43;
+            this.lblTarih1.Text = "ToLongDateString()";
+            // 
+            // lblTarih2
+            // 
+            this.lblTarih2.AutoSize = true;
+            this.lblTarih2.BackColor = System.Drawing.Color.White;
+            this.lblTarih2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTarih2.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.lblTarih2.Location = new System.Drawing.Point(314, 288);
+            this.lblTarih2.Name = "lblTarih2";
+            this.lblTarih2.Size = new System.Drawing.Size(116, 15);
+            this.lblTarih2.TabIndex = 44;
+            this.lblTarih2.Text = "ToLongDateString()";
+            // 
+            // lblTarihveSonrasiniAciklama1
+            // 
+            this.lblTarihveSonrasiniAciklama1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTarihveSonrasiniAciklama1.AutoSize = true;
+            this.lblTarihveSonrasiniAciklama1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTarihveSonrasiniAciklama1.ForeColor = System.Drawing.Color.Black;
+            this.lblTarihveSonrasiniAciklama1.Location = new System.Drawing.Point(490, 331);
+            this.lblTarihveSonrasiniAciklama1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTarihveSonrasiniAciklama1.Name = "lblTarihveSonrasiniAciklama1";
+            this.lblTarihveSonrasiniAciklama1.Size = new System.Drawing.Size(350, 15);
+            this.lblTarihveSonrasiniAciklama1.TabIndex = 45;
+            this.lblTarihveSonrasiniAciklama1.Text = "SEÇMEK İSTEDİĞİNİZ KUFAÖRÜ LÜTFEN AŞAĞIDAN SEÇİNİZ";
+            // 
+            // btnRandevularım
+            // 
+            this.btnRandevularım.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnRandevularım.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnRandevularım.BackColor = System.Drawing.Color.Red;
+            this.btnRandevularım.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRandevularım.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnRandevularım.ForeColor = System.Drawing.Color.White;
+            this.btnRandevularım.Location = new System.Drawing.Point(751, 100);
+            this.btnRandevularım.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnRandevularım.Name = "btnRandevularım";
+            this.btnRandevularım.Size = new System.Drawing.Size(101, 31);
+            this.btnRandevularım.TabIndex = 48;
+            this.btnRandevularım.Text = "Randevularım";
+            this.btnRandevularım.UseVisualStyleBackColor = false;
+            this.btnRandevularım.Click += new System.EventHandler(this.btnRandevularım_Click);
+            this.btnRandevularım.MouseLeave += new System.EventHandler(this.btnRandevularım_MouseLeave);
+            this.btnRandevularım.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btnRandevularım_MouseMove);
+            // 
+            // btnCikis
+            // 
+            this.btnCikis.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnCikis.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnCikis.BackColor = System.Drawing.Color.Red;
+            this.btnCikis.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCikis.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCikis.ForeColor = System.Drawing.Color.White;
+            this.btnCikis.Location = new System.Drawing.Point(856, 100);
+            this.btnCikis.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnCikis.Name = "btnCikis";
+            this.btnCikis.Size = new System.Drawing.Size(66, 31);
+            this.btnCikis.TabIndex = 47;
+            this.btnCikis.Text = "Çıkış";
+            this.btnCikis.UseVisualStyleBackColor = false;
+            this.btnCikis.Click += new System.EventHandler(this.btnCikis_Click);
+            this.btnCikis.MouseLeave += new System.EventHandler(this.btnCikis_MouseLeave);
+            this.btnCikis.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btnCikis_MouseMove);
+            // 
+            // lblTarihveSonrasiniAciklama2
+            // 
+            this.lblTarihveSonrasiniAciklama2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTarihveSonrasiniAciklama2.AutoSize = true;
+            this.lblTarihveSonrasiniAciklama2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTarihveSonrasiniAciklama2.ForeColor = System.Drawing.Color.Black;
+            this.lblTarihveSonrasiniAciklama2.Location = new System.Drawing.Point(314, 303);
+            this.lblTarihveSonrasiniAciklama2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTarihveSonrasiniAciklama2.Name = "lblTarihveSonrasiniAciklama2";
+            this.lblTarihveSonrasiniAciklama2.Size = new System.Drawing.Size(258, 15);
+            this.lblTarihveSonrasiniAciklama2.TabIndex = 49;
+            this.lblTarihveSonrasiniAciklama2.Text = "Seçmek İstediğiniz Kufaörün üzerine Tıklayınız";
+            // 
+            // Hakan
+            // 
+            this.Hakan.AutoSize = true;
+            this.Hakan.BackColor = System.Drawing.Color.White;
+            this.Hakan.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Hakan.ForeColor = System.Drawing.Color.DarkRed;
+            this.Hakan.Location = new System.Drawing.Point(567, 413);
+            this.Hakan.Name = "Hakan";
+            this.Hakan.Size = new System.Drawing.Size(136, 16);
+            this.Hakan.TabIndex = 93;
+            this.Hakan.Text = "HAKAN YALNIÇKAYA";
+            // 
+            // Çalışan1
+            // 
+            this.Çalışan1.BackColor = System.Drawing.Color.DimGray;
+            this.Çalışan1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Çalışan1.Location = new System.Drawing.Point(542, 396);
+            this.Çalışan1.Name = "Çalışan1";
+            this.Çalışan1.Size = new System.Drawing.Size(183, 58);
+            this.Çalışan1.TabIndex = 69;
+            this.Çalışan1.TabStop = false;
+            this.Çalışan1.Click += new System.EventHandler(this.picBoxBeyinveSinirCerrahisi_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::Randevu_Sistemi.Properties.Resources.Screenshot_8;
+            this.ClientSize = new System.Drawing.Size(1015, 670);
+            this.Controls.Add(this.Hakan);
+            this.Controls.Add(this.Çalışan1);
+            this.Controls.Add(this.lblTarihveSonrasiniAciklama2);
+            this.Controls.Add(this.btnRandevularım);
+            this.Controls.Add(this.btnCikis);
+            this.Controls.Add(this.lblTarihveSonrasiniAciklama1);
+            this.Controls.Add(this.lblTarih2);
+            this.Controls.Add(this.lblTarih1);
+            this.Controls.Add(this.llblTarihAciklama);
+            this.Controls.Add(this.picBoxTarihAciklama);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form3";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kuaför Seçim Ekranı";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form3_FormClosed);
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxTarihAciklama)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Çalışan1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Panel panel2;
+        public System.Windows.Forms.MonthCalendar monthCalendar_randevuTarihleri;
+        public System.Windows.Forms.PictureBox picBoxTarihAciklama;
+        public System.Windows.Forms.Label llblTarihAciklama;
+        public System.Windows.Forms.Label lblTarih1;
+        public System.Windows.Forms.Label lblTarih2;
+        public System.Windows.Forms.Label lblTarihveSonrasiniAciklama1;
+        public System.Windows.Forms.Button btnRandevularım;
+        public System.Windows.Forms.Button btnCikis;
+        public System.Windows.Forms.Label lblTarihveSonrasiniAciklama2;
+        public System.Windows.Forms.Label Hakan;
+        public System.Windows.Forms.PictureBox Çalışan1;
+        public System.Windows.Forms.Label lblHosgeldinizAciklama;
+    }
+}
---
+++ Form3.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Randevu_Sistemi
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+        public string secilenTarih;
+
+        Form4 frm4 = new Form4();
+
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+            Hakan.Text = "HAKAN YALÇINKAYA";   
+            Form1 frm1 = (Form1)Application.OpenForms["Form1"];
+            Form2 frm2 = (Form2)Application.OpenForms["Form2"];
+
+            lblHosgeldinizAciklama.Text = "Sn.  " + frm1.ad + " " + frm1.soyad + ", e-randevu sistemine hoşgeldiniz.";
+
+            monthCalendar_randevuTarihleri.MinDate = DateTime.Now;
+            monthCalendar_randevuTarihleri.MaxDate = new DateTime(2021, 12, 31);
+
+            DateTime frm2deSecilenTarih = frm2.monthCalendar_randevuTarihleri.SelectionStart.Date;
+
+            lblTarih1.Text = frm2deSecilenTarih.ToLongDateString();
+            lblTarih2.Text = frm2deSecilenTarih.ToLongDateString();
+
+            if (DateTime.Compare(frm2.monthCalendar_randevuTarihleri.SelectionRange.Start, DateTime.Today.Date) == 1)
+                monthCalendar_randevuTarihleri.SelectionStart = frm2.monthCalendar_randevuTarihleri.SelectionStart;
+        }
+
+        private void btnRandevularım_Click(object sender, EventArgs e)
+        {
+            this.Refresh();
+            this.Visible = false;
+
+            Form frm8 = new Form8();
+            frm8.ShowDialog();
+        }
+
+        private void monthCalendar_randevuTarihleri_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            secilenTarih = e.Start.ToShortDateString();
+
+            DateTime frm3deSecilenTarih = monthCalendar_randevuTarihleri.SelectionStart.Date;
+
+            lblTarih1.Text = frm3deSecilenTarih.ToLongDateString();
+            lblTarih2.Text = frm3deSecilenTarih.ToLongDateString();
+        }
+
+        private void picBoxBeyinveSinirCerrahisi_Click(object sender, EventArgs e)
+        {
+           
+                frm4.lblBransAdi1.Text = "HAKAN YALÇINKAYA";
+             
+                frm4.lblBolumİsmi.Text = Hakan.Text;
+
+                frm4.lblBos.Text = Hakan.Text;
+                frm4.lblDolu.Text = Hakan.Text;
+
+                this.Refresh();
+
+                this.Visible = false;
+
+                frm4.ShowDialog();
+           
+        }
+        private void Form3_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnCikis_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+            Application.Restart();
+        }
+
+        private void btnRandevularım_MouseLeave(object sender, EventArgs e)
+        {
+            btnRandevularım.BackColor = Color.Red;
+        }
+
+        private void btnRandevularım_MouseMove(object sender, MouseEventArgs e)
+        {
+            btnRandevularım.BackColor = SystemColors.WindowFrame;
+        }
+
+        private void btnCikis_MouseMove(object sender, MouseEventArgs e)
+        {
+            btnCikis.BackColor = SystemColors.WindowFrame;
+        }
+
+        private void btnCikis_MouseLeave(object sender, EventArgs e)
+        {
+            btnCikis.BackColor = Color.Red;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
